4980edbfdbcea810f1b8d9301db40b5392595fef
Mike Lee <mike.lee@partnerstack.com>
2022-11-27 16:52:40 -0500

    Add lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,8 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
-    // override/add rules settings here, such as:
-    "vue/no-unused-vars": "error",
+    // 0 = off, 1 = warn, 2 = error
+    "vue/no-unused-vars": 1,
+    "vue/multi-word-component-names": 1,
   },
 };
